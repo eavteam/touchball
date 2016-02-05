a96b5fd9b6e1211f4be0b943c5459dffd108e0d0
nepeyvoda vladimir <nepeyvodava@gmail.com>
2016-02-05 11:18:51 +0600

    first commit

---
+++ desktop/src/com/eavteam/touthball/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eavteam.touthball.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eavteam.touthball.TouthBall;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TouthBall(), config);
+	}
+}
