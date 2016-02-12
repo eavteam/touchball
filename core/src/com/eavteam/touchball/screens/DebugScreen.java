0d6aeb0c9066a6aa02295100b9e023de9d0e2620
aion89 <aion89@bk.ru>
2016-02-12 23:18:34 +0600

    bu

--- core/src/com/eavteam/touchball/screens/DebugScreen.java
+++ core/src/com/eavteam/touchball/screens/DebugScreen.java
@@ -89,6 +89,7 @@ public class DebugScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
